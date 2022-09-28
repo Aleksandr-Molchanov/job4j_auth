9d4116d4a0fedf7d5ea7ac1423b8298ff332b1a5
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2022-09-28 11:25:21 +0300

    6. DTO [#504800]

--- src/main/java/ru/job4j/auth/controller/EmployeeController.java
+++ src/main/java/ru/job4j/auth/controller/EmployeeController.java
@@ -1,16 +1,16 @@
 package ru.job4j.auth.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.auth.model.Employee;
 import ru.job4j.auth.model.Person;
 import ru.job4j.auth.service.EmployeeService;
+import ru.job4j.auth.service.PersonService;
 
 import java.sql.Timestamp;
 import java.util.Set;
@@ -24,12 +24,18 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    private static final String API = "http://localhost:8080/person/";
+    private final PersonService personService;
 
-    private static final String API_ID = "http://localhost:8080/person/{id}";
+    private BCryptPasswordEncoder encoder;
 
-    public EmployeeController(final EmployeeService employeeService) {
+    private static final String API = "http://localhost:8080/users/";
+
+    private static final String API_ID = "http://localhost:8080/users/{id}";
+
+    public EmployeeController(final EmployeeService employeeService, BCryptPasswordEncoder encoder, PersonService personService) {
         this.employeeService = employeeService;
+        this.encoder = encoder;
+        this.personService = personService;
     }
 
     @GetMapping("/")
@@ -78,6 +84,20 @@ public class EmployeeController {
         employee.setId(id);
         this.employeeService.delete(employee);
         return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/example2")
+    public Person example2(@RequestBody Person person) {
+        Person current = personService.findById(person.getId()).get();
+        if (person.getUsername() == null || person.getPassword() == null) {
+            throw new NullPointerException("Username and password mustn't be empty");
+        }
+        if (person.getPassword().length() < 6) {
+            throw new IllegalArgumentException("Invalid password. Password length must be more than 5 characters.");
+        }
+        current.setUsername(person.getUsername());
+        current.setPassword(encoder.encode(person.getPassword()));
+        return this.personService.save(current);
     }
 
     private void checkingInputData(@RequestBody Employee employee) {
