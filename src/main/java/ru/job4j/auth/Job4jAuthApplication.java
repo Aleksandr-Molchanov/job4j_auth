ada27d6aca9b8163f0c3fd8514df166b442bded0
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2022-09-29 15:42:46 +0300

    4. Обработка исключений и Spring REST [#504797 #345762]

--- src/main/java/ru/job4j/auth/Job4jAuthApplication.java
+++ src/main/java/ru/job4j/auth/Job4jAuthApplication.java
@@ -1,14 +1,20 @@
 package ru.job4j.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
-public class Job4jAuthApplication {
+public class Job4jAuthApplication extends SpringBootServletInitializer {
 
     @Bean
     public RestTemplate getTemplate() {
@@ -23,6 +29,19 @@ public class Job4jAuthApplication {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Job4jAuthApplication.class);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
     }
 
     public static void main(String[] args) {
--- src/main/resources/db/2_insert_data.sql
+++ src/main/resources/db/2_insert_data.sql
@@ -1,5 +1,5 @@
 insert into employee (name, surname, inn) values ('Petr', 'Arsentev', 11111111);
 insert into employee (name, surname, inn) values ('Aleksaandr', 'Molchanov', 2222222);
 
-insert into person (username, password, employee_id) values ('parsentev', '$2a$10$738i5ZVXx/FwZPvqLKpXVu6rWHHQl2vWzMFlTnPNuWPaduoHsWXAC', 5);
-insert into person (username, password, employee_id) values ('amolchanov', '$2a$10$738i5ZVXx/FwZPvqLKpXVu6rWHHQl2vWzMFlTnPNuWPaduoHsWXAC', 6);
+insert into person (username, password, employee_id) values ('parsentev', '$2a$10$738i5ZVXx/FwZPvqLKpXVu6rWHHQl2vWzMFlTnPNuWPaduoHsWXAC', 1);
+insert into person (username, password, employee_id) values ('amolchanov', '$2a$10$738i5ZVXx/FwZPvqLKpXVu6rWHHQl2vWzMFlTnPNuWPaduoHsWXAC', 2);
